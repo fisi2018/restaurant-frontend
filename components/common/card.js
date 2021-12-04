b27f831924edd27fd63164261c87b247dceca626
Marco <marco.fura@unmsm.edu.pe>
2021-12-03 23:03:24 -0500

    next auth url added

--- components/common/card.js
+++ components/common/card.js
@@ -6,7 +6,7 @@ import { useOrderContext } from "../../stateManagement/contexts/OrderContext";
 import { useState } from "react";
 import { FaQuoteLeft } from "react-icons/fa";
 export default function Card({el,category}){
-    const {index,orders,addElement}=useOrderContext();
+    const {addElement}=useOrderContext();
     const [info,setInfo]=useState(false);
     const {push}=useRouter();
     return(
@@ -18,9 +18,7 @@ export default function Card({el,category}){
         push("/account/pedidos")}} id="add" className="absolute transition duration-300 text-yellow-500 text-xl hover:text-white z-40 p-2 rounded-full hover:bg-yellow-500 top-2 bg-white right-0 translate-x-9">
                 <BiRestaurant/>
             </span>
-            {/*<div className={`absolute w-full h-full bg-white top-0 transform  ${info ? "translate-y-0":"-translate-y-full"}`} >
-                    <p>{el.description}</p>
-    </div>*/}
+            
             <article className="transiton bg-cover bg-center w-full h-full flex flex-col justify-start items-center py-6 px-8  duration-300 ">
                 {info && <>
                 <span className="text-3xl mb-6" >
